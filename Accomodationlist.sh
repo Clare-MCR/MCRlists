2629da3a9be0ece557ed2cae065b92cbd49d4b48
Richard Gunning <rjgunning@gmail.com>
2015-08-28 10:08:09 +0100

    update link

--- Accomodationlist.sh
+++ Accomodationlist.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -o gradinfo.csv https://cweb4.clare.cam.ac.uk/rooms/hens-owng6732sdsalsdjhbewq/gradinfo.csv
+curl -o gradinfo.csv https://cweb1.clare.cam.ac.uk/rooms/hens-owng6732sdsalsdjhbewq/gradinfo.csv
